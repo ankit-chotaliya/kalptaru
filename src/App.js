f45cded3875c607f710591d3cb28707a42bbc0ce
Ankit3110200 <chotaliyaankit31@gmail.com>
2022-04-15 12:33:08 +0530

    App.js changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from './Components/NavBar/Navbar';
 import NewOrder from './Components/NewOrder/NewOrder';
 import Test from './Components/Test/Test';
 import NotFound from './Components/NotFound/NotFound';
 import Home from './Components/Home/Home';
+import AddClient from './Components/AddClient/AddClient';
+
 import Editorder from './Components/EditOrder/editOrder';
+
 
 const App = () => {
   return (
@@ -19,6 +22,7 @@ const App = () => {
         <Route path="/create" exact element={<NewOrder/>}/>
         <Route path="/EditOrder" exact element={<Editorder/>}/>
         <Route path="/home" exact element={<Home/>}/>
+        <Route path="/modal" exact element={<AddClient/>}/>
       </Routes>
 
     </>
