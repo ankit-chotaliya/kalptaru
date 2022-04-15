7a86c14af332da9380635fd67d87847a181e545a
ParthGoti <parthgoti999@gmail.com>
2022-04-15 18:39:54 +0530

    Editorder web done

--- src/Components/EditOrder/editOrder.jsx
+++ src/Components/EditOrder/editOrder.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../NavBar/Navbar';
+import { BiArrowBack } from "react-icons/bi";
+import './editOrder.css';
+import { editData } from './editData';
 
 function editOrder() {
   return (
-    <div>Hello</div>
+    <>
+      <Navbar />
+      <div style={{'width':'160px'}} className=" mt-4 editbackbtn d-flex " id='editbackbtn'>
+        <Link to='../Home'>
+          <BiArrowBack id='backbtn'/>
+        </Link>
+        <p className='editorder-text' id='editorder-text' >Edit Order</p>
+      </div>
+
+      <div className='container' id="container">
+        <ui className='editorder-data' id="editorder-data">
+          {editData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName} id="editOrder">
+                <div id='clientdiv'>
+                  Client Name:
+                  <text id='cname'>{item.cname}</text>
+                </div>
+                <div id='refnodiv'>
+                  Ref No:
+                  <text  id='rno'>{item.rno}</text>
+                </div>
+                <div id='editicon'><Link style={{ color: 'black' }} to={'#'}>{item.icon}</Link></div>
+                <hr style={{ 'marginTop': '30px', 'zIndex':'0' }}></hr>
+              </li>
+            );
+          })}
+        </ui>
+      </div>
+
+    </>
   )
 }
 
-export default editOrder
+export default editOrder;
