8f58bdcf5579256603c5ad521adc509ce7cdd2bd
sakshisjain <jainsaku153@gmail.com>
2022-04-15 20:04:14 +0530

    Home Screen Completed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,75 +13,77 @@ function Home() {
         <div className="row">
           <div className="col-md-2"></div>
           <div className="col-md-8 colcenter-home">
-            <div className="title-home mt-5">Order Status</div>
-            <div className="row contentaround-home">
-              <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
-                <div className="card box-home ">
-                  <div className="card-body">
-                    <img className="logo-home" src={newOrder} alt="New Order" />
-                    <h5 className="card-title boxname-home">New Order</h5>
+            <div className="parent_box-home"> <div className="title-home mt-5">Order Status</div></div>
+            <div className="parent_box-home">
+              <div className="row contentaround-home ">
+                <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
+                  <div className="card box-home">
+                    <div className="card-body box-body ">
+                      <img className="logo-home" src={newOrder} alt="New Order" />
+                      <h5 className="card-title boxname-home">New Order</h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
-                <div className="card box-home ">
-                  <div className="card-body">
-                    <img
-                      className="logo-home"
-                      src={editOrder}
-                      alt="Edit Order"
-                    />
-                    <h5 className="card-title boxname-home">Edit Order</h5>
+                <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
+                  <div className="card box-home ">
+                    <div className="card-body box-body">
+                      <img
+                        className="logo-home"
+                        src={editOrder}
+                        alt="Edit Order"
+                      />
+                      <h5 className="card-title boxname-home">Edit Order</h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
-                <div className="card box-home ">
-                  <div className="card-body">
-                    <img
-                      className="logo-home"
-                      src={trackOrder}
-                      alt="Track Order"
-                    />
-                    <h5 className="card-title boxname-home">Track Order</h5>
+                <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
+                  <div className="card box-home ">
+                    <div className="card-body box-body">
+                      <img
+                        className="logo-home"
+                        src={trackOrder}
+                        alt="Track Order"
+                      />
+                      <h5 className="card-title boxname-home">Track Order</h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
-                <div className="card box-home ">
-                  <div className="card-body">
-                    <img
-                      className="logo-home"
-                      src={completedOrders}
-                      alt="New Order"
-                    />
-                    <h5 className="card-title boxname-home">
-                      Completed Orders
-                    </h5>
+                <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
+                  <div className="card box-home ">
+                    <div className="card-body box-body">
+                      <img
+                        className="logo-home"
+                        src={completedOrders}
+                        alt="New Order"
+                      />
+                      <h5 className="card-title boxname-home">
+                        Completed Orders
+                      </h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
-                <div className="card box-home ">
-                  <div className="card-body">
-                    <img
-                      className="logo-home"
-                      src={sendReminder}
-                      alt="Edit Order"
-                    />
-                    <h5 className="card-title boxname-home">Send Reminder</h5>
+                <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
+                  <div className="card box-home ">
+                    <div className="card-body box-body">
+                      <img
+                        className="logo-home"
+                        src={sendReminder}
+                        alt="Edit Order"
+                      />
+                      <h5 className="card-title boxname-home">Send Reminder</h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
-                <div className="card box-home ">
-                  <div className="card-body">
-                    <img
-                      className="logo-home"
-                      src={urgentOrders}
-                      alt="Track Order"
-                    />
-                    <h5 className="card-title boxname-home">Urgent Orders</h5>
+                <div className="col-md-4 col-xs-6 col-sm-6 boxcenter-home">
+                  <div className="card box-home ">
+                    <div className="card-body box-body">
+                      <img
+                        className="logo-home"
+                        src={urgentOrders}
+                        alt="Track Order"
+                      />
+                      <h5 className="card-title boxname-home">Urgent Orders</h5>
+                    </div>
                   </div>
                 </div>
               </div>
