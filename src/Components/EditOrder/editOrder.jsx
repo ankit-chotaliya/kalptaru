8659d952f06a078fb2a0c14d13b4f72617028110
Ankit3110200 <chotaliyaankit31@gmail.com>
2022-04-17 15:25:21 +0530

    ListView Component Created

--- src/Components/EditOrder/editOrder.jsx
+++ src/Components/EditOrder/editOrder.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
-
+import Navbar from '../NavBar/Navbar'
+import {AiOutlineArrowLeft} from 'react-icons/ai'
+import { FiEdit2 } from 'react-icons/fi'
+import './editOrder.css';
+import ListView from '../Helper/ListView/ListView';
 function editOrder() {
   return (
-    <div>Hello</div>
+    <>
+    <Navbar/>
+        <div className='container no-main no-border pageview'>
+            <div className='no-heading'>
+              <AiOutlineArrowLeft/> Edit Order
+            </div>
+            
+            <div className='eo-container mt-4'>
+                <ListView
+                property1="Client Name: "
+                property2="Ref No: "
+                value1="Parth Goti"
+                value2="1234"
+                icon={<FiEdit2/>}
+                />
+                <ListView
+                property1="Client Name: "
+                property2="Ref No: "
+                value1="Parth Goti"
+                value2="1234"
+                icon={<FiEdit2/>}
+                />
+                <ListView
+                property1="Client Name: "
+                property2="Ref No: "
+                value1="Parth Goti"
+                value2="1234"
+                icon={<FiEdit2/>}
+                />
+                <ListView
+                property1="Client Name: "
+                property2="Ref No: "
+                value1="Parth Goti"
+                value2="1234"
+                icon={<FiEdit2/>}
+                />
+                <ListView
+                property1="Client Name: "
+                property2="Ref No: "
+                value1="Parth Goti"
+                value2="1234"
+                icon={<FiEdit2/>}
+                />
+            </div>
+           
+        </div>
+        </>
   )
 }
 
