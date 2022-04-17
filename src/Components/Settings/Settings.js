bf0a11b999f957bc30d416d2938536816b0cba57
Ankit3110200 <chotaliyaankit31@gmail.com>
2022-04-17 12:59:47 +0530

    Settings Component done

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -4,8 +4,12 @@ import AddKarigar from '../AddKarigar/AddKarigar';
 import './Settings.css';
 import Navbar from '../NavBar/Navbar';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { FiEdit3 } from 'react-icons/fi'
 
 const Settings = () => {
+    const [mobileNo,setMobileNo]=useState("1234567890");
+    const [password,setPassWord]=useState("Abc@2022");
+    const [showPassword,setShowPassWord]=useState(false);
     const [addClientModal,setAddClientModal]=useState(false);
     const [addKarigarModal,setAddKarigarModal]=useState(false);
 
@@ -18,6 +22,11 @@ const Settings = () => {
         e.preventDefault();
         setAddKarigarModal(true);
     }
+
+    const handlepassword=(e)=>{
+        setShowPassWord(true);
+        setPassWord(e.target.value);
+    }
   return (
     <>
     <Navbar/>
@@ -27,39 +36,51 @@ const Settings = () => {
             </div>
             
             <div className='co-container mt-4'>
-                <div className='st-mobile'>
+                <div className='st-mobile mt-2'>
                     <label htmlFor='st-mob'>Phone No.</label>
                     <br/>
+                    <div className='st-mob-sub'>
                     <input 
                     type="text"
+                    className='st-mob-input'
                     placeholder='Mobile Number'
-                    value="8866140344"
+                    value={mobileNo}
+                    onChange={(e)=>{setMobileNo(e.target.value)}}
                     id="st-mob"
                     />
+                    <FiEdit3 className='st-mob-icon'/>
+                    </div>
                 </div>
-            <div className='st-password'>
-                <label htmlFor='st-pass'>Password</label>
-                <br/>
-                <input 
-                type="text"
-                placeholder='Password'
-                value="Abc@2022"
-                id="st-pass"
-                />
-            </div>
-                <div className='co-customer-share mt-2'>
+                <div className='st-mobile mt-2'>
+                    <label htmlFor='st-pass'>Password</label>
+                    <br/>
+                    <div className='st-mob-sub'>
+                    <input 
+                    type={showPassword?"text":"password"}
+                    className='st-mob-input'
+                    placeholder='Password'
+                    value={password}
+                    onChange={handlepassword}
+                    onFocus={()=>{setShowPassWord(true)}}
+                    onBlur={()=>{setShowPassWord(false)}}
+                    id="st-pass"
+                    />
+                    <FiEdit3 className='st-mob-icon'/>
+                    </div>
+                </div>
+                <div className='co-customer-share mt-4'>
                   
                    <button className='co-share-btn' onClick={handleClient}>
-                        Add Karigar
+                        Add Client
                    </button>
                    <AddClient
                     show={addClientModal}
                     onHide={() => setAddClientModal(false)}
                     />
                 </div>
-                <div className='co-karigar-share mt-2'>
+                <div className='co-karigar-share mt-4'>
                     <button className='co-share-btn' onClick={handleAddKarigar}>
-                        Add Client
+                        Add Karigar
                    </button>
                    <AddKarigar
                     show={addKarigarModal}
