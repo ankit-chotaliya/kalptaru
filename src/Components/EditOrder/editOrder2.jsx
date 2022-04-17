1534afb54fe2e60034ca0d550bbaf8870ef7e747
sakshisjain <jainsaku153@gmail.com>
2022-04-17 15:59:08 +0530

    editOrder2

--- src/Components/EditOrder/editOrder2.jsx
+++ src/Components/EditOrder/editOrder2.jsx
@@ -5,49 +5,57 @@ import { Link } from 'react-router-dom';
 import { BiArrowBack } from "react-icons/bi";
 import { HiOutlineTrash } from "react-icons/hi";
 import { VscEdit } from "react-icons/vsc";
+import {GrPrevious, GrNext} from "react-icons/gr";
 import ring from "./ring.jpg";
-
-
+import { Helmet } from "react-helmet";
+import $ from 'jquery';
 function editOrder2() {
 
-    let slideIndex = 1;
-    showSlides(slideIndex);
 
-    // Next/previous controls
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+    $(document).ready(function () {
 
-    // Thumbnail image controls
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
+        $('.items').slick({
+            dots: true,
+            infinite: true,
+            speed: 800,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
 
-    function showSlides(n) {
-        let i;
-        if (document.getElementsByClassName("mySlides") && document.getElementsByClassName("demo") && document.getElementById("caption")) {
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("demo");
-            let captionText = document.getElementById("caption");
-            if (n > slides.length) { slideIndex = 1 }
-            if (n < 1) { slideIndex = slides.length }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-            captionText.innerHTML = dots[slideIndex - 1].alt;
-        }
-    }
-
+            ]
+        });
+    });
 
     return (
         <>
             <Navbar />
-
+            <Helmet>
+            </Helmet>
             <div className="container-fluid ">
                 <div className="row">
                     <div className="col-md-2"></div>
@@ -63,14 +71,6 @@ function editOrder2() {
                         </div>
                         <div className='clientdetails mt-5 mx-5'>
                             <p className='clienttitle'>Client Details</p>
-                            {/* <div className='clientdata'>
-                                <p>
-                                    Client Name: Harsh Panchal<br />
-                                    Phone No: 12356789<br />
-                                    Email ID: person@gmail.com
-                                </p>
-                            </div> */}
-
                             <div className='table-responsive-md clientdata'>
                                 <table>
                                     <tr>
@@ -91,86 +91,20 @@ function editOrder2() {
 
                             <p className='clienttitle'>Order Details</p>
 
-                            <div class="container">
 
-
-                                <a class="prev" onclick="plusSlides(-1)">Prev</a>
-                                <a class="next" onclick="plusSlides(1)">Next</a>
-
-
-                                <div class="row">
-                                    <div class="column">
-                                        <img class="demo cursor" src={ring} style={{ width: '20%' }} onclick="currentSlide(1)" alt="The Woods" />
-                                    </div>
-                                    <div class="column">
-                                        <img class="demo cursor" src={ring} style={{ width: '20%' }} onclick="currentSlide(2)" alt="Cinque Terre" />
-                                    </div>
-                                    <div class="column">
-                                        <img class="demo cursor" src={ring} style={{ width: '20%' }} onclick="currentSlide(3)" alt="Mountains and fjords" />
-                                    </div>
-                                    <div class="column">
-                                        <img class="demo cursor" src={ring} style={{ width: '20%' }} onclick="currentSlide(4)" alt="Northern Lights" />
-                                    </div>
-                                    <div class="column">
-                                        <img class="demo cursor" src={ring} style={{ width: '20%' }} onclick="currentSlide(5)" alt="Nature and sunrise" />
-                                    </div>
-                                    <div class="column">
-                                        <img class="demo  cursor" src={ring} style={{ width: '20%' }} onclick="currentSlide(6)" alt="Snowy Mountains" />
-                                    </div>
-                                </div>
+                            <div className='slider'>
+                            <button className='btn-slider'><GrPrevious></GrPrevious></button>
+                            <img className='img-slider' src={ring}></img>
+                            <img className='img-slider' src={ring}></img>
+                            <img className='img-slider' src={ring}></img>
+                            <img className='img-slider' src={ring}></img>
+                            <button className='btn-slider'><GrNext></GrNext></button>
+                            
                             </div>
+                        
 
 
 
-
-
-                            {/* <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                                <ol class="carousel-indicators">
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                </ol>
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <div className="row">
-                                            <div className="col-md-3">
-                                                <div className="single-box">
-                                                    <div className="img-area">
-                                                        <img class="d-block w-100" src={ring} alt="First slide" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-3">
-                                                <div className="single-box">
-                                                    <div className="img-area">
-                                                        <img class="d-block w-100" src={ring} alt="Second slide" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-3">
-                                                <div className="single-box">
-                                                    <div className="img-area">
-                                                        <img class="d-block w-100" src={ring} alt="Third slide" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-3">
-                                                <div className="single-box">
-                                                    <div className="img-area">
-                                                        <img class="d-block w-100" src={ring} alt="Forth slide" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-3">
-                                                <div className="single-box">
-                                                    <div className="img-area">
-                                                        <img class="d-block w-100" src={ring} alt="Fifth slide" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> */}
 
                             <div className='table-responsive-md clientdata'>
                                 <table>
