443b9b5b7f9d92edefec5f588e3d04754a44eeed
Ankit3110200 <chotaliyaankit31@gmail.com>
2022-05-04 13:54:42 +0530

    kargiar componenet updated by goti

--- src/Components/AddKarigar/AddKarigar.js
+++ src/Components/AddKarigar/AddKarigar.js
@@ -9,6 +9,8 @@ const AddKarigar = (props) => {
     const [karigarState,setKarigarState]=useState("");
     const [karigarCity,setKarigarCity]=useState("");
     const [karigarPincode,setKarigarPincode]=useState("");
+    const [karigarEmail,setKarigarEmail]=useState("");
+    const [karigarMobile,setKarigarMobile]=useState("");
 
     const AddKarigarSubmit=(e)=>{
         e.preventDefault();
@@ -65,6 +67,32 @@ const AddKarigar = (props) => {
                     </div>
                 </div>
             </div>
+            <div className=' row'>
+              <div className="col-md-6 col-sm-12 mt-4">
+              <label >Karigar Email (Optional)</label>
+                  <div className='d-flex justify-content-start'>
+                  <input 
+                  type="text" 
+                  className="form-control no-input" 
+                  placeholder='Email Address'
+                  value={karigarEmail}
+                  onChange={(e)=>{setKarigarEmail(e.target.value)}}
+                  />
+                  </div>
+              </div>
+              <div className="col-md-6 col-sm-12 mt-4">
+              <label >Karigar Mobile No.</label>
+                  <div className='d-flex justify-content-start'>
+                  <input 
+                  type="text" 
+                  className="form-control no-input" 
+                  placeholder='Mobile Number'
+                  value={karigarMobile}
+                  onChange={(e)=>{setKarigarMobile(e.target.value)}}
+                  />
+                  </div>
+              </div>
+          </div>
             <div className=' row'>
                 <div className="col-md-6 col-sm-12 mt-4">
                 <label htmlFor="ref-num">Country</label>
